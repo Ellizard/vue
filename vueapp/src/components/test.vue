f85ed992436ee31e67b09734a23cc29f2033d6d3
Artem <artem.ostavnenko@ffwagency.com>
2017-04-06 17:28:53 +0300

    vueap

---
+++ vueapp/src/components/test.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="test">
+    <h1>
+      {{ title }}
+    </h1>
+  </div>
+</template>
+
+
+
+<script>
+  export default {
+    name: 'test',
+    data() {
+      return {
+        title: 'test hello',
+        user: {
+          name: 'John',
+          lastname: 'Doe',
+
+        }
+      }
+    }
+  }
+</script>
+
+
+<style scoped>
+
+</style>
