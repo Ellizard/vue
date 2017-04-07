958a0f3833a592168623a86a8278bafe93909b79
Artem <artem.ostavnenko@ffwagency.com>
2017-04-07 15:20:27 +0300

    vue

--- vueapp/src/components/test.vue
+++ vueapp/src/components/test.vue
@@ -1,8 +1,32 @@
 <template>
   <div class="test">
+    <input type="text" v-model="title">
     <h1>
       {{ title }}
     </h1>
+    <p v-if="showName">
+      {{user.name}}
+    </p>
+    <ul>
+      <li v-for="item in items"> {{item.title}} </li>
+    </ul>
+    <button v-on:click="greet">Say hello</button>
+    <input type="text" v-on:keyup="pressKey" v-on:keyup.enter="enterHit">
+
+    <br>
+    <br>
+
+    <label>
+      First name:
+    </label>
+    <input type="text" v-model="user.name">
+    <br>
+    <label>
+          Last name:
+        </label>
+        <input type="text" v-model="user.lastname">
+        <br>
+    <h3> {{fullName}} </h3>
   </div>
 </template>
 
@@ -17,10 +41,34 @@
         user: {
           name: 'John',
           lastname: 'Doe',
+        },
+        showName: true,
+        items: [
+          {title: 'Item 1'},
+          {title: 'Item 2'},
+          {title: 'Item 3'},
+          {title: 'Item 4'}
+        ]
+      }
 
-        }
+    },
+    methods: {
+      greet: function(){
+        alert('Hello');
+      },
+      pressKey: function(e){
+        console.log(e.target.value);
+      },
+      enterHit: function(){
+        console.log('Enter');
+      }
+    },
+    computed: {
+      fullName: function(){
+        return this.user.name+ ' '+ this.user.пше lastname
       }
     }
+
   }
 </script>
 
